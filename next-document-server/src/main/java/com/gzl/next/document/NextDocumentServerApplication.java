4e0ba4995105acc337eaa051277978e69c34765e
gaozhilai <1049106781@qq.com>
2019-09-28 13:51:00 +0800

    初始化仓库

---
+++ next-document-server/src/main/java/com/gzl/next/document/NextDocumentServerApplication.java
@@ -0,0 +1,13 @@
+package com.gzl.next.document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextDocumentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextDocumentServerApplication.class, args);
+	}
+
+}
