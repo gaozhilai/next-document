0737748f3037272a210656097178eb85e744bcc9
gaozhilai <1049106781@qq.com>
2019-10-07 18:16:29 +0800

    增加websocket同步画板数据接口

--- next-document-server/src/main/java/com/gzl/next/document/controller/websocket/WebSocketController.java
+++ next-document-server/src/main/java/com/gzl/next/document/controller/websocket/WebSocketController.java
@@ -1,9 +1,7 @@
 package com.gzl.next.document.controller.websocket;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gzl.next.document.pojo.vo.DrawingBoardVO;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -19,6 +17,12 @@ public class WebSocketController {
     @GetMapping("/socket/push/{sid}/{message}")
     public String pushMessage(@PathVariable String sid, @PathVariable String message) throws IOException {
         WebSocketServer.sendInfo(message, sid);
+        return "success";
+    }
+
+    @RequestMapping("/drawing")
+    public String pushDrawing(@RequestBody DrawingBoardVO param) throws IOException {
+        WebSocketServer.sendInfo(param.getBase64Data(), null);
         return "success";
     }
 }
---
+++ next-document-server/src/main/java/com/gzl/next/document/pojo/vo/DrawingBoardVO.java
@@ -0,0 +1,12 @@
+package com.gzl.next.document.pojo.vo;
+
+import lombok.Data;
+
+/**
+ * @author GaoZhilai
+ * @date 19/10/3
+ */
+@Data
+public class DrawingBoardVO {
+    private String base64Data;
+}
